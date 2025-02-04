c1d4201e6b26b02b78b162d9ace80efd9e854ad8
david_smith <david_smith@sweetwater.com>
2025-02-04 10:30:06 -0500

    main Created `src/Helpers/DataModel.php`.

---
+++ src/Helpers/DataModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Zerotoprod\CurlHelperSdk\Helpers;
+
+use Zerotoprod\Transformable\Transformable;
+
+class DataModel
+{
+    use \Zerotoprod\DataModel\DataModel;
+    use Transformable;
+}
