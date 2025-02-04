e96613d451afdf061a1d4d2200b1ebd47627bdf9
david_smith <david_smith@sweetwater.com>
2025-02-04 10:18:41 -0500

    feat/sdk Updated `src/CurlHelperSdk.php`.

--- src/CurlHelperSdk.php
+++ src/CurlHelperSdk.php
@@ -2,7 +2,15 @@
 
 namespace Zerotoprod\CurlHelperSdk;
 
+use Zerotoprod\CurlHelper\CurlHelper;
+
 class CurlHelperSdk
 {
-
+    /**
+     * Returns the headers of a request as an array.
+     */
+    public static function parseHeaders(string $response, int $header_size): array
+    {
+        return CurlHelper::parseHeaders($response, $header_size);
+    }
 }
